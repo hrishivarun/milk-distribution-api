226e7644587864652a8ed1df33b537e8966eaca6
Hrishi <varun.hrishi.18@gmail.com>
2022-08-14 22:06:10 +0530

    deploy

--- server.js
+++ server.js
@@ -17,7 +17,9 @@ mongoose.connect(connectionURL, {
 const app = express();
 
 
-app.use(express.static('./'));
+app.get('/', (req, res) => {
+    res.sendFile(__dirname+'/index.html');
+});
 app.use(addOrder);
 app.use(updateDetails);
 app.use(updateStatus);
